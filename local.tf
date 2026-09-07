24f06263de56292c1f9699dd1a16d5740e6b4d91
krish-sky <3dstudent1997@gmail.com>
2026-09-07 17:16:43 +0530

    mm

--- local.tf
+++ local.tf
@@ -18,4 +18,11 @@ locals {
           },
           var.igw_tags
     )
+
+    az_names = slice(data.aws_availability_zones.available.names, 0, 2 )
+
+  public_subnet_tags = var.public_subnet_tags
+
+
+
 }
